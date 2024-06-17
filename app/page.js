944140cc40f94f01b104ac36285bb75cd2f5655b
aghp81 <aghp81@gmail.com>
2024-06-17 23:07:02 +0330

    ایجاد Dashboard

--- app/page.js
+++ app/page.js
@@ -1,9 +1,7 @@
-import React from 'react'
-
-const page = () => {
+const Dashboard = () => {
   return (
-    <div>page</div>
+    <div>Dashboard</div>
   )
 }
 
-export default page
+export default Dashboard
