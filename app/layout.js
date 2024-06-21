e24b3da9b8f8ff249fdb6b54d186cb083bab0985
aghp81 <aghp81@gmail.com>
2024-06-21 13:30:47 +0330

    قرار دادن  children  در div جداگانه

--- app/layout.js
+++ app/layout.js
@@ -18,8 +18,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <div className="flex flex-col h-screen max-h-screen">
         <Nav />
-        {children}
+        <div>{children}</div>
+        </div>
       </body>
     </html>
   );
