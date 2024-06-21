ff63a083707f55f2b6efd9c8bdf0b9a8a47442ff
aghp81 <aghp81@gmail.com>
2024-06-21 13:27:10 +0330

    config.autoAddCss = false

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,8 @@ import { Inter } from "next/font/google";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 
+config.autoAddCss = false; // کتابخانه اصلی SVG Font Awesome سعی نکند عناصر <style> را در <head> صفحه وارد کند.
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
