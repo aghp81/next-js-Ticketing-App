3e435aa6e067302fd16d62935721ed75efac33d6
aghp81 <aghp81@gmail.com>
2024-06-21 13:22:14 +0330

    import fortawesome

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,9 @@
-import { Inter } from "next/font/google";
 import Nav from "./(components)/Nav";
 import "./globals.css";
+import { Inter } from "next/font/google";
+
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
