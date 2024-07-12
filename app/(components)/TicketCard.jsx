cad85146d29f0cd00001d5975116969dab8967d6
aghp81 <aghp81@gmail.com>
2024-07-12 12:23:44 +0330

    add PriorityDispaly component to TicketCard

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -1,9 +1,11 @@
 import DeleteBlock from "./DeleteBlock";
+import PriorityDispaly from "./PriorityDispaly";
 
 const TicketCard = () => {
   return (
     <div>
       <DeleteBlock />
+      <PriorityDispaly />
     </div>
   );
 };
