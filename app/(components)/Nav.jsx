8e9e103331c4c77fb80ca62e20e90c223e3538df
aghp81 <aghp81@gmail.com>
2024-07-12 11:12:51 +0330

    className for nav

--- app/(components)/Nav.jsx
+++ app/(components)/Nav.jsx
@@ -3,19 +3,21 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 
 const Nav = () => {
-  return <nav>
-    <div>
+  return (
+    <nav className="flex justify-between bg-nav p-4">
+      <div className="flex items-center space-x-4">
         <Link href="/">
-            <FontAwesomeIcon icon={faHome} className="icon" />
+          <FontAwesomeIcon icon={faHome} className="icon" />
         </Link>
         <Link href="/TicketPage/new">
-            <FontAwesomeIcon icon={faTicket} className="icon" />
+          <FontAwesomeIcon icon={faTicket} className="icon" />
         </Link>
-    </div>
-    <div>
+      </div>
+      <div>
         <p className="text-default-text">aghp81@gmail.com</p>
-    </div>
-  </nav>;
-}
+      </div>
+    </nav>
+  );
+};
 
-export default Nav
+export default Nav;
