59b40a56c0fe93707a8a78a381ac88194f7636e3
aghp81 <aghp81@gmail.com>
2024-07-12 12:08:56 +0330

    use TicketCard in dashboard

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,11 @@
+import TicketCard from "./(components)/TicketCard";
+
 const Dashboard = () => {
   return (
-    <div className="p-3">داشبورد</div>
-  )
-}
+    <div className="p-3">
+      <TicketCard />
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
