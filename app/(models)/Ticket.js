7e50fcff9c13d037af9d6516280af400a9f5ed86
aghp81 <aghp81@gmail.com>
2024-07-17 18:43:07 +0330

    Ticket Model

--- app/(models)/Ticket.js
+++ app/(models)/Ticket.js
@@ -4,7 +4,17 @@ mongoose.connect(process.env.MONGODB_URI);
 mongoose.Promise = global.Promise;
 
 const ticketSchema = new Schema(
-    {
-        
-    }
-)
+  {
+    title: String,
+    descriptin: String,
+    category: String,
+    priority: Number,
+    progress: Number,
+    status: String,
+    active: Boolean,
+  },
+  {
+    timestamps: true,
+  }
+);
+
