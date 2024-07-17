692c7570b3151a945f32c1259d4808221cd08fcf
aghp81 <aghp81@gmail.com>
2024-07-17 18:54:54 +0330

    try catch show error

--- app/api/route.js
+++ app/api/route.js
@@ -1,6 +1,9 @@
 import Ticket from "../(models)/Ticket";
 import { NextResponse } from "next/server";
 
-export async function POST(req){
-    
+export async function POST(req) {
+  try {
+  } catch (error) {
+    return NextResponse.json({ message: "خطا", error }, { status: 500 });
+  }
 }
