b27c52eabb050fecd6e8d34a8ea1dfd652c8a541
aghp81 <aghp81@gmail.com>
2024-07-17 19:14:00 +0330

    use client & const startingTicketData

--- app/(components)/TicketForm.jsx
+++ app/(components)/TicketForm.jsx
@@ -1,7 +1,18 @@
+// next is server side By default.
+"use client"; // because use the form
+
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 
 const TicketForm = () => {
+  const startingTicketData = {
+    title: "",
+    description: "",
+    priority: 1,
+    progress: 0,
+    status: "شروع نشده",
+    category: "مشکل سخت افزاری",
+  };
   return <div>TicketForm</div>;
 };
 
