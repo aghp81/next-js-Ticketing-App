b174ef80b46cc667b9ef16eec4c3b43e4b2f3336
aghp81 <aghp81@gmail.com>
2024-07-19 10:35:51 +0330

    اصلاح مسیرها برای رفع خطا

--- app/(components)/TicketForm.jsx
+++ app/(components)/TicketForm.jsx
@@ -19,7 +19,7 @@ const TicketForm = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const res = await fetch("api/Tickets", {
+    const res = await fetch("/api/Tickets", {
       method: "POST",
       body: JSON.stringify({formData}),
       "content-type": "application/json"
