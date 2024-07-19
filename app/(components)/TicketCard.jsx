43b731588481f6ba4dd674ec8b2fb43b921fbb62
aghp81 <aghp81@gmail.com>
2024-07-19 20:54:34 +0330

    فراخوانی اطلاعات تیکت از دیتابیس
    فراخوانی اطلاعات تیکت از دیتابیس

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -3,7 +3,7 @@ import PriorityDispaly from "./PriorityDispaly";
 import ProgressDisplay from "./ProgressDisplay";
 import StatusDisplay from "./StatusDisplay";
 
-const TicketCard = ({ticket}) => {
+const TicketCard = ({ ticket }) => {
   return (
     <div className="flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2">
       <div className="flex mb-3">
@@ -12,15 +12,15 @@ const TicketCard = ({ticket}) => {
           <DeleteBlock />
         </div>
       </div>
-      <h4>عنوان تیکت</h4>
+      <h4>{ticket.title}</h4>
       <hr className="h-px border-0 bg-page mb-2" />
       <p className="whitespace-pre-wrap">
-        این توضیحات تیکت است. لطفا آن را انجام بده
+      {ticket.description}
       </p>
       <div className="flex-grow"></div>
       <div className="flex mt-2">
         <div className="flex flex-col">
-          <p className="text-xs my-1">08/31/23 10:43PM</p>
+          <p className="text-xs my-1">{ticket.createdAt}</p>
           <ProgressDisplay />
         </div>
         <div className="mr-auto flex items-end">
