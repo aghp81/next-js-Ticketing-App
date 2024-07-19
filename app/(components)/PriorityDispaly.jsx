ac097e1c8e99651ef2c01f68960bfe3b19cb116c
aghp81 <aghp81@gmail.com>
2024-07-19 11:52:22 +0330

    تغییر رنگ آیکون faFire متناسب با مقدار اولویت

--- app/(components)/PriorityDispaly.jsx
+++ app/(components)/PriorityDispaly.jsx
@@ -4,14 +4,26 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const PriorityDispaly = ({ priority }) => {
   return (
     <div className="flex justify-start align-baseline">
-      <FontAwesomeIcon 
+      <FontAwesomeIcon
         icon={faFire}
         className={`pr-1 ${priority > 0 ? "text-red-400" : "text-slate-400"} `}
-     />
-      <FontAwesomeIcon icon={faFire} className="text-red-400" />
-      <FontAwesomeIcon icon={faFire} className="text-red-400" />
-      <FontAwesomeIcon icon={faFire} className="text-red-400" />
-      <FontAwesomeIcon icon={faFire} className="text-red-400" />
+      />
+      <FontAwesomeIcon
+        icon={faFire}
+        className={`pr-1 ${priority > 1 ? "text-red-400" : "text-slate-400"} `}
+      />
+      <FontAwesomeIcon
+        icon={faFire}
+        className={`pr-1 ${priority > 2 ? "text-red-400" : "text-slate-400"} `}
+      />
+      <FontAwesomeIcon
+        icon={faFire}
+        className={`pr-1 ${priority > 3 ? "text-red-400" : "text-slate-400"} `}
+      />
+      <FontAwesomeIcon
+        icon={faFire}
+        className={`pr-1 ${priority > 4 ? "text-red-400" : "text-slate-400"} `}
+      />
     </div>
   );
 };
