7ac7d120edc7598ea9b0cacb8cf499f3764cdb40
aghp81 <aghp81@gmail.com>
2024-07-19 11:47:58 +0330

    if priority > 0 show icon red else show another

--- app/(components)/PriorityDispaly.jsx
+++ app/(components)/PriorityDispaly.jsx
@@ -1,10 +1,13 @@
 import { faFire } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const PriorityDispaly = ({priority}) => {
+const PriorityDispaly = ({ priority }) => {
   return (
     <div className="flex justify-start align-baseline">
-      <FontAwesomeIcon icon={faFire} className="text-red-400" />
+      <FontAwesomeIcon 
+        icon={faFire}
+        className={`pr-1 ${priority > 0 ? "text-red-400" : "text-slate-400"} `}
+     />
       <FontAwesomeIcon icon={faFire} className="text-red-400" />
       <FontAwesomeIcon icon={faFire} className="text-red-400" />
       <FontAwesomeIcon icon={faFire} className="text-red-400" />
