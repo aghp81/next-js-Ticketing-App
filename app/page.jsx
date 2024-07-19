d447a5941f7d1ea51e85be00849c3044dbc4322b
aghp81 <aghp81@gmail.com>
2024-07-19 11:37:43 +0330

    نمایش تیکت ها بر اساس دسته بندی در صفحه اصلی

--- app/page.jsx
+++ app/page.jsx
@@ -24,15 +24,24 @@ const Dashboard = async () => {
   return (
     <div className="p-5">
       <div>
-        <div className="lg:grid grid-cols-2 xl:grid-cols-4">
-          {/* اگر تیکت وجود دارد و کتگوری آن یونیک است */}
-          {tickets &&
-            uniqueCategories?.map((uniqueCategory, categoryIndex) => (
-              <div key={categoryIndex} className="mb-4">
-                
+        {/* اگر تیکت وجود دارد و کتگوری آن یونیک است */}
+        {tickets &&
+          uniqueCategories?.map((uniqueCategory, categoryIndex) => (
+            <div key={categoryIndex} className="mb-4">
+              <h2>{uniqueCategory}</h2>
+              <div className="lg:grid grid-cols-2 xl:grid-cols-4">
+                {tickets
+                  .filter((ticket) => ticket.category === uniqueCategory)
+                  .map((filteredTicket, _index) => (
+                    <TicketCard
+                      id={_index}
+                      key={_index}
+                      ticket={filteredTicket}
+                    />
+                  ))}
               </div>
-            ))}
-        </div>
+            </div>
+          ))}
       </div>
     </div>
   );
