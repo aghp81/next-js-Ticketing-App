0618fe75394d0891e787f633703b1c38453d7a9b
aghp81 <aghp81@gmail.com>
2024-07-19 11:18:22 +0330

    // نمایش تیکتها در صفحه اصلی از دیتابیس

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,22 @@
 import TicketCard from "./(components)/TicketCard";
 
+// نمایش تیکتها در صفحه اصلی از دیتابیس
+const getTickets = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/Tickets", {
+      cache: "no-store",
+    });
+
+    return res.json();
+  } catch (error) {
+    console.log("خطا در دریافت تیکت ها", error);
+  }
+};
+
 const Dashboard = () => {
+
+  // نمایش تیکتها در صفحه اصلی از دیتابیس
+  const {tickets} = await getTickets()
   return (
     <div className="p-5">
       <div className="lg:grid grid-cols-2 xl:grid-cols-4">
