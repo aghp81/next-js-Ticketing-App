293c6768ffdc2c459ef9f167d2f32dbe68544e09
aghp81 <aghp81@gmail.com>
2024-07-20 20:24:41 +0330

    format timestamp

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -4,6 +4,17 @@ import ProgressDisplay from "./ProgressDisplay";
 import StatusDisplay from "./StatusDisplay";
 
 const TicketCard = ({ ticket }) => {
+  // format timestamp
+  const formatTimeStamp = (timestamp) => {
+    const options = {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
+    };
+  };
   return (
     <div className="flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2">
       <div className="flex mb-3">
@@ -14,9 +25,7 @@ const TicketCard = ({ ticket }) => {
       </div>
       <h4>{ticket.title}</h4>
       <hr className="h-px border-0 bg-page mb-2" />
-      <p className="whitespace-pre-wrap">
-      {ticket.description}
-      </p>
+      <p className="whitespace-pre-wrap">{ticket.description}</p>
       <div className="flex-grow"></div>
       <div className="flex mt-2">
         <div className="flex flex-col">
