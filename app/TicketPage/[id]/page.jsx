4f1ebb309e00b8f3071ed9d46d063768aee8ceba
aghp81 <aghp81@gmail.com>
2024-07-26 19:53:38 +0330

    return res.json

--- app/TicketPage/[id]/page.jsx
+++ app/TicketPage/[id]/page.jsx
@@ -1,14 +1,16 @@
 import TicketForm from "@/app/(components)/TicketForm";
 
 const getTicketById = async (id) => {
-  try {
-    const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
-      cache: "no-store"
-    })
-  } catch (error) {
-    console.log(error)
+  const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("تیکت دریافت نشد");
   }
-}
+
+  return res.json();
+};
 
 const TicketPage = async ({ params }) => {
   // If the ID of the ticket already exists, the ticket should be edited, otherwise, the ticket is new
