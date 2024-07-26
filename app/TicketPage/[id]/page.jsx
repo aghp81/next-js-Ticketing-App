a3493660a882f437ba5b076cf8f888649b7637ac
aghp81 <aghp81@gmail.com>
2024-07-26 19:47:16 +0330

    If the ID of the ticket already exists

--- app/TicketPage/[id]/page.jsx
+++ app/TicketPage/[id]/page.jsx
@@ -1,6 +1,13 @@
 import TicketForm from "@/app/(components)/TicketForm";
 
-const TicketPage = ({ params }) => {
+const TicketPage = async ({ params }) => {
+  // If the ID of the ticket already exists, the ticket should be edited, otherwise, the ticket is new
+  const EDITMODE = params.id === "new" ? flase : true;
+  let updateTicketData = {}; // blank object
+  if (EDITMODE) {
+    updateTicketData = await getTicketById(params.id);
+    console.log(updateTicketData);
+  }
   return <TicketForm />;
 };
 
