06103ddf661c6982e8c66bb17b70bf5c8d3cd422
aghp81 <aghp81@gmail.com>
2024-07-26 19:23:53 +0330

    اصلاح کدها

--- app/api/Tickets/[id]/route.js
+++ app/api/Tickets/[id]/route.js
@@ -5,7 +5,7 @@ export async function DELETE(req, { params }) {
   try {
     const { id } = params;
     await Ticket.findByIdAndDelete(id);
-    return NextResponse.json({ message: "تیکت حذف شد.", error }, { status: 200 });
+    return NextResponse.json({ message: "تیکت حذف شد." }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ message: "خطا", error }, { status: 500 });
   }
