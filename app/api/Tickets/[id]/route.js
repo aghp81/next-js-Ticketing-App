258c56ce2053d0f28b1ff1dccc0ff6bb27ce4325
aghp81 <aghp81@gmail.com>
2024-07-26 18:49:37 +0330

    try catch error

--- app/api/Tickets/[id]/route.js
+++ app/api/Tickets/[id]/route.js
@@ -1,10 +1,9 @@
 import Ticket from "@/app/models/Ticket";
 import { NextResponse } from "next/server";
 
-export async function DELETE(req, {params}){
-    try {
-        
-    } catch (error) {
-        
-    }
+export async function DELETE(req, { params }) {
+  try {
+  } catch (error) {
+    return NextResponse.json({ message: "خطا", error }, { satatus: 500 });
+  }
 }
