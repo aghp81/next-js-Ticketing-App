86ceb5f62b213d8e34d9614b5ebd7020f9fe39e9
aghp81 <aghp81@gmail.com>
2024-07-26 19:51:05 +0330

    getTicketById

--- app/TicketPage/[id]/page.jsx
+++ app/TicketPage/[id]/page.jsx
@@ -1,5 +1,15 @@
 import TicketForm from "@/app/(components)/TicketForm";
 
+const getTicketById = async (id) => {
+  try {
+    const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
+      cache: "no-store"
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 const TicketPage = async ({ params }) => {
   // If the ID of the ticket already exists, the ticket should be edited, otherwise, the ticket is new
   const EDITMODE = params.id === "new" ? flase : true;
