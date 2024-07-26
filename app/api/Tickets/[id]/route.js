3bf0d0c50ee0b6712da153bdf4d9fcd9673249ea
aghp81 <aghp81@gmail.com>
2024-07-26 19:56:27 +0330

    get ticket id for edit ticket

--- app/api/Tickets/[id]/route.js
+++ app/api/Tickets/[id]/route.js
@@ -1,6 +1,13 @@
 import Ticket from "@/app/(models)/Ticket";
 import { NextResponse } from "next/server";
 
+// for edit ticket
+export async function GET(req, { params }) {
+  const { id } = params;
+
+  const foundTicket = await Ticket.findOne({ _id: id });
+}
+
 export async function DELETE(req, { params }) {
   try {
     const { id } = params;
