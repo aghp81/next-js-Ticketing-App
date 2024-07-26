a804fb19f027b310e39e64ec509b58445a17ac80
aghp81 <aghp81@gmail.com>
2024-07-26 19:37:14 +0330

    import Link from "next/link"

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -2,6 +2,7 @@ import DeleteBlock from "./DeleteBlock";
 import PriorityDispaly from "./PriorityDispaly";
 import ProgressDisplay from "./ProgressDisplay";
 import StatusDisplay from "./StatusDisplay";
+import Link from "next/link";
 
 const TicketCard = ({ ticket }) => {
   // format timestamp
@@ -27,20 +28,20 @@ const TicketCard = ({ ticket }) => {
           <DeleteBlock id={ticket._id} />
         </div>
       </div>
-      <Link href={`/TicketPage/${ticket._id}`} style={}>
-      <h4>{ticket.title}</h4>
-      <hr className="h-px border-0 bg-page mb-2" />
-      <p className="whitespace-pre-wrap">{ticket.description}</p>
-      <div className="flex-grow"></div>
-      <div className="flex mt-2">
-        <div className="flex flex-col">
-          <p className="text-xs my-1">{formatTimeStamp(ticket.createdAt)}</p>
-          <ProgressDisplay progress={ticket.progress} />
+      <Link href={`/TicketPage/${ticket._id}`} style={{ display: "contents" }}>
+        <h4>{ticket.title}</h4>
+        <hr className="h-px border-0 bg-page mb-2" />
+        <p className="whitespace-pre-wrap">{ticket.description}</p>
+        <div className="flex-grow"></div>
+        <div className="flex mt-2">
+          <div className="flex flex-col">
+            <p className="text-xs my-1">{formatTimeStamp(ticket.createdAt)}</p>
+            <ProgressDisplay progress={ticket.progress} />
+          </div>
+          <div className="mr-auto flex items-end">
+            <StatusDisplay status={ticket.status} />
+          </div>
         </div>
-        <div className="mr-auto flex items-end">
-          <StatusDisplay status={ticket.status} />
-        </div>
-      </div>
       </Link>
     </div>
   );
