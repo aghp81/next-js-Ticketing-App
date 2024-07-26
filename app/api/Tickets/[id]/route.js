0b3913b5f743231a44121270f249f8c63880b8ab
aghp81 <aghp81@gmail.com>
2024-07-26 18:51:11 +0330

    Ticket.findByIdAndDelete

--- app/api/Tickets/[id]/route.js
+++ app/api/Tickets/[id]/route.js
@@ -3,6 +3,8 @@ import { NextResponse } from "next/server";
 
 export async function DELETE(req, { params }) {
   try {
+    const { id } = params;
+    await Ticket.findByIdAndDelete(id);
   } catch (error) {
     return NextResponse.json({ message: "خطا", error }, { satatus: 500 });
   }
