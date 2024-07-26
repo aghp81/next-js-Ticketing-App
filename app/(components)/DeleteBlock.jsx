8598f03fb53e0cbd7551d47bfb910a443759a279
aghp81 <aghp81@gmail.com>
2024-07-26 19:00:33 +0330

    useRouter from "next/navigation"

--- app/(components)/DeleteBlock.jsx
+++ app/(components)/DeleteBlock.jsx
@@ -1,7 +1,10 @@
 import { faX } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRouter } from "next/navigation";
 
 const DeleteBlock = (id) => {
+  const router = useRouter;
+  
   const deleteTicket = async () => {
     const res = await fetch(`http://localhost:3000/api/Tickets/${id}`,{
       method: "DELETE"
