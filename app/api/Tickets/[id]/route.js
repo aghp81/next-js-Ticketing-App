ccc675cfd27a33c82e249d6e8d336952ee09479f
aghp81 <aghp81@gmail.com>
2024-07-26 18:52:09 +0330

    تیکت حذف شد.

--- app/api/Tickets/[id]/route.js
+++ app/api/Tickets/[id]/route.js
@@ -5,6 +5,7 @@ export async function DELETE(req, { params }) {
   try {
     const { id } = params;
     await Ticket.findByIdAndDelete(id);
+    return NextResponse.json({ message: "تیکت حذف شد.", error }, { satatus: 200 });
   } catch (error) {
     return NextResponse.json({ message: "خطا", error }, { satatus: 500 });
   }
