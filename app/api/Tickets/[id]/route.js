f7ee253b6e1890b7c2dfda724fc94c30d600a149
aghp81 <aghp81@gmail.com>
2024-07-26 18:47:30 +0330

    export async function DELETE

--- app/api/Tickets/[id]/route.js
+++ app/api/Tickets/[id]/route.js
@@ -1,3 +1,10 @@
 import Ticket from "@/app/models/Ticket";
 import { NextResponse } from "next/server";
 
+export async function DELETE(req, {params}){
+    try {
+        
+    } catch (error) {
+        
+    }
+}
