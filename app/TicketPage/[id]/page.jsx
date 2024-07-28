250242678eb626b6c460465c92ccbb2d4f58c120
aghp81 <aghp81@gmail.com>
2024-07-28 13:08:23 +0330

    updateTicketData.foundTicket

--- app/TicketPage/[id]/page.jsx
+++ app/TicketPage/[id]/page.jsx
@@ -18,9 +18,13 @@ const TicketPage = async ({ params }) => {
   let updateTicketData = {}; // blank object
   if (EDITMODE) {
     updateTicketData = await getTicketById(params.id);
-    console.log(updateTicketData);
+    updateTicketData = updateTicketData.foundTicket;
+  } else {
+    updateTicketData = {
+      _id: "new",
+    };
   }
-  return <TicketForm />;
+  return <TicketForm ticket={updateTicketData} />;
 };
 
 export default TicketPage;
